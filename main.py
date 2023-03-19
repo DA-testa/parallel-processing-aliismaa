817877daff603a93bc43d2ce50d53c9fad414bac
Alisa Mamikova <116195263+aliismaa@users.noreply.github.com>
2023-03-19 19:46:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 # python3
 import heapq
-
-
+# n - thread count 
+# m - job count
+# data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
 n, m = map(int, input().split())
-times = list(map(int, input().split()))
+data = list(map(int, input().split()))
 
 
 threads = [(0, i) for i in range(n)]
 
 for i in range(m):
 
-    job_time = times[i]
+    job = data[i]
 
-    start_time, thread_id = heapq.heappop(threads)
+    start_time, thread_index = heapq.heappop(threads)
+    
+# TODO: print out the results, each pair in it's own line
+    print(thread_index, start_time)
 
-    print(thread_id, start_time)
+    start_time += job
 
-    start_time += job_time
-
-    heapq.heappush(threads, (start_time, thread_id))
-
-
+    heapq.output(threads, (start_time, thread_index))
